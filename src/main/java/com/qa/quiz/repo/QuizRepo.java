8ab820eaa3a00d67b4ad9c4b4cb3684ed9e93643
Andrew-McCall <amccall@qa.com>
2021-06-11 16:07:48 +0100

    AM: QSP-30 QSP-31 > Created Quiz & Question Repo interfaces (No Customer JPA Functions Yet)

---
+++ src/main/java/com/qa/quiz/repo/QuizRepo.java
@@ -0,0 +1,11 @@
+package com.qa.quiz.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.qa.quiz.domain.Question;
+
+@Repository
+public interface QuizRepo extends JpaRepository<Question, Long>{
+	
+}
