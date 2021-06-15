27f014a98fabe7cf7046c90c4fd65867240f022a
Andrew-McCall <amccall@qa.com>
2021-06-15 15:04:08 +0100

    AM: QSP-40 > Quiz Controller

--- src/main/java/com/qa/quiz/rest/QuizController.java
+++ src/main/java/com/qa/quiz/rest/QuizController.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,45 +25,31 @@ public class QuizController {
 		this.service = service;
 	}
 
-		@PostMapping("/create")
-		public ResponseEntity<Quiz> createQuiz(@RequestBody Quiz quiz) {
-			return new ResponseEntity<Quiz>(this.service.createQuiz(quiz), HttpStatus.CREATED);
-		}
-		
-		@GetMapping("/getAll")
-		public ResponseEntity<List<Quiz>> getQuiz() {
-			return ResponseEntity.ok(this.service.getQuiz());
-		}
+	@PostMapping("/create")
+	public ResponseEntity<Quiz> createQuiz(@RequestBody Quiz quiz) {
+		return new ResponseEntity<Quiz>(this.service.createQuiz(quiz), HttpStatus.CREATED);
+	}
+	
+	@GetMapping("/getAll")
+	public ResponseEntity<List<Quiz>> getQuiz() {
+		return ResponseEntity.ok(this.service.getQuiz());
+	}
 
-		
-//		@GetMapping("/getCar/{id}")
-//		public Car getPenguinById(@PathVariable Long id) {
-//			return this.service.getCarById(id);
-//		}
-//
-//		@GetMapping("/getCarByModel/{model}")
-//		public Car getCarByModel(@PathVariable String model) {
-//			return this.service.getCarByModel(model);
-//		}
-//		@GetMapping("/getCarByMake/{make}")
-//		public Car getCarByMake(@PathVariable String make) {
-//			return this.service.getCarByModel(make);
-//		}
-//		@GetMapping("/getCarByColour/{colour}")
-//		public Car getCarByColour(@PathVariable String colour) {
-//			return this.service.getCarByModel(colour);
-//		}
-//
-//		@DeleteMapping("/delete/{id}")
-//		public ResponseEntity<Boolean> removePenguin(@PathVariable Long id) {
-//			return this.service.removeCar(id) ? ResponseEntity.ok(true): new ResponseEntity<Boolean>(false, HttpStatus.INTERNAL_SERVER_ERROR);
-//		}
-//
-//		@PutMapping("/update/{id}")
-//		public Car updatePenguin(@PathVariable Long id, @RequestBody Car car) {
-//			return this.service.updateCar(id, car);
-//		}
-//	}
-		
-		
+	@GetMapping("/getQuiz/{id}")
+	public Quiz getQuizById(@PathVariable Long id) {
+		return this.service.getQuizById(id);
+	}
+
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<Boolean> removeQuiz(@PathVariable Long id) {
+		return this.service.removeQuiz(id) ? ResponseEntity.ok(true): new ResponseEntity<Boolean>(false, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@PutMapping("/update/{id}")
+	public Quiz Quiz(@PathVariable Long id, @RequestBody Quiz quiz) {
+		return this.service.updateQuiz(id, quiz);
+	}
+	
 }
+		
+
