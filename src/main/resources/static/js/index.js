1e7c8d2983b8b828e0c8fafcede32aca27af2c01
Andrew-McCall <amccall@qa.com>
2021-06-17 11:13:27 +0100

    AM: QSP-20 > Added Quiz Delete Button

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -26,6 +26,23 @@ getAll()
 
 }
 
+async function deleteQuiz(i){
+  fetch("./quiz/delete/"+i, { //1
+    method: 'delete', //2
+    headers: {
+    "Content-type": "application/json" //3
+    },
+    body:null})
+    .then(res => res.json())
+    .then((data)=>{getAll()})
+    .catch((error) => console.log(`Request failed ${error}`))
+}
+
+async function createQuestion(i){
+  console.log(i);
+}
+
+
 async function getAll(){
 fetch("./quiz/getAll", { //1
 method: 'get', //2
@@ -40,10 +57,41 @@ body: null
 const element = document.getElementById("dynamicDiv");
 element.innerHTML = '';
 data.forEach(item => {
-  let child = document.createElement("p");
-  child.setAttribute("class", "border")
-  child.innerHTML = JSON.stringify(item);
-  element.appendChild(child);
+  let parent = document.createElement("div");
+  parent.setAttribute("class", "border");
+
+  let title = document.createElement("div");
+  //name
+  let child = document.createElement("h4");
+  child.innerHTML = item.name;
+  title.appendChild(child);
+  //desc
+  child = document.createElement("h6");
+  child.innerHTML = item.description;
+  title.appendChild(child);
+
+  parent.appendChild(title);
+  
+  item.questions.forEach(quest =>{
+    let question = document.createElement("h7");
+    question.innerHTML = `Q: ${quest.Question}\nA:${quest.answer}`;
+    parent.appendChild(question);
+  })
+
+  let button = document.createElement("button");
+  button.innerHTML = "New Question";
+  button.setAttribute("class","btn btn-secondary");
+  button.setAttribute("onClick", `createQuestion(${item.id})`)
+  element.appendChild(button);
+
+  button = document.createElement("button");
+  button.innerHTML = "Delete";
+  button.setAttribute("class","btn btn-danger");
+  button.setAttribute("onClick", `deleteQuiz(${item.id})`)
+  element.appendChild(button);
+
+  element.appendChild(parent);
+  element.appendChild(document.createElement("br"));
 });
 
 })
